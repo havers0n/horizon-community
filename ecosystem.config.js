1072b01a80aaf639a6334bd707b69e42c77d1fe6
havers0n <danypetrov2002@gmail.com>
2025-08-03 01:42:23 +0300

    FIX: обавлен node_args для принудительной загрузки переменных окружения перед инициализацией Supabase

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,10 @@ module.exports = {
   apps: [{
     name: 'roleplayidentity', // Имя процесса, которое вы используете
     script: './dist/apps/server/main.js', // <-- ПРАВИЛЬНЫЙ ПУТЬ
+    
+    // ЭТА СТРОКА - РЕШЕНИЕ ВСЕЙ ПРОБЛЕМЫ
+    node_args: "-r dotenv/config",
+    
     // Эта опция говорит PM2 автоматически загружать переменные из .env
     // при наличии установленной библиотеки dotenv
     // Убедитесь, что 'dotenv' есть в ваших dependencies в package.json
